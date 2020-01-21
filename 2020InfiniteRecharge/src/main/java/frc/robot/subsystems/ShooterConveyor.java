01ee6405d0689ef6fdb8a7bb2ec24d8a28b6ad57
Emma Prokop <emma@Emmas-MacBook-Air.local>
2020-01-20 19:47:36 -0500

    Added intake and conveyor.

---
+++ 2020InfiniteRecharge/src/main/java/frc/robot/subsystems/ShooterConveyor.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class ShooterConveyor extends SubsystemBase {
+
+    private final CANSparkMax m_motor;
+
+	public ShooterConveyor () {
+        m_motor = new CANSparkMax(Constants.SHOOTER_CONVEYOR_SPARK, MotorType.kBrushless);
+    } 
+
+    public void moveConveyor(){
+        m_motor.set(1);
+    }
+}
