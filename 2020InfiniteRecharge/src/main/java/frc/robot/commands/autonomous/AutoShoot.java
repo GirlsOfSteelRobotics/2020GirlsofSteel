de6fdd80f80120cbf47ccec208454056ea324a63
Emma Prokop <emma@Emmas-MacBook-Air.local>
2020-01-27 18:16:12 -0500

    Added reuired subsystems to autoshoot command

--- 2020InfiniteRecharge/src/main/java/frc/robot/commands/autonomous/AutoShoot.java
+++ 2020InfiniteRecharge/src/main/java/frc/robot/commands/autonomous/AutoShoot.java
@@ -2,32 +2,40 @@ package frc.robot.commands.autonomous;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.ShooterConveyor;
 import frc.robot.OI;
 import frc.robot.Robot;
 
 public class AutoShoot extends CommandBase {
 	Command autonomousCommand;
 	Shooter shooter;
+	ShooterConveyor shooterConveyor;
 	OI OI;
 
+	private double m_rpm;
+	private double m_time;
 
-	public AutoShoot() {
-		requires(Robot.Shooter);
+	public AutoShoot(Shooter shooter, ShooterConveyor shooterConveyor, double rpm, double time) {
+		this.shooter = shooter;
+		this.shooterConveyor = shooterConveyor;
+		m_rpm = rpm;
+		m_time = time;
+		//requires(Robot.Shooter);
 		shooter = Robot.shooter;
 		OI = Robot.OI;
 		// Use requires() here to declare subsystem dependencies
-        //super.addRequirements(Shooter); When a subsystem is written, add the requires line back in.
+		super.addRequirements(shooter); 
+		super.addRequirements(shooterConveyor);
 	}
 
-    private void requires(Object shooter2) {
-	}
+    //private void requires(Object shooter2) {}
 
 	public void initialize() {
     }
 
 	// Called repeatedly when this Command is scheduled to run
 	public void execute() { 
-		if(OI.XboxController.)
+		//if(OI.XboxController.)
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
