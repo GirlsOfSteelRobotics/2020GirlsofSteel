25a43585f364101d64bc6df21d0ea0f2da51760a
PJ Reiniger <pj.reiniger@gmail.com>
2020-02-04 22:14:40 -0500

    Adding skeleton for superstructure widget

---
+++ 2020InfiniteRechargeDashboard/src/main/java/com/gos/infinite_recharge/sd_widgets/super_structure/SuperStructureController.java
@@ -0,0 +1,35 @@
+package com.gos.infinite_recharge.sd_widgets.super_structure;
+
+import com.gos.infinite_recharge.sd_widgets.super_structure.data.ControlPanelData;
+import com.gos.infinite_recharge.sd_widgets.super_structure.data.LiftData;
+import com.gos.infinite_recharge.sd_widgets.super_structure.data.ShooterConveyorData;
+import com.gos.infinite_recharge.sd_widgets.super_structure.data.ShooterIntakeData;
+import com.gos.infinite_recharge.sd_widgets.super_structure.data.ShooterWheelsData;
+import com.gos.infinite_recharge.sd_widgets.super_structure.data.WinchData;
+import javafx.fxml.FXML;
+
+public class SuperStructureController {
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    public void updateShooterConveyor(ShooterConveyorData shooterConveyorData) {
+    }
+
+    public void updateShooterIntake(ShooterIntakeData shooterIntakeData) {
+    }
+
+    public void updateShooterWheels(ShooterWheelsData shooterWheelsData) {
+    }
+
+    public void updateControlPanel(ControlPanelData controlPanelData) {
+    }
+
+    public void updateWinch(WinchData winchData) {
+    }
+
+    public void updateLift(LiftData liftData) {
+    }
+}
