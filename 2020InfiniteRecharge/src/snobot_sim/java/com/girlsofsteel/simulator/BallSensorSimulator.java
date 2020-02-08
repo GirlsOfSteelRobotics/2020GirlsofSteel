abccd8fb17af18f8303e76eb5871af7856bd4157
Meghna Behari <meghnabehari@meghnas-air.wifi.cmu.edu>
2020-02-08 11:20:18 -0500

    ballConveyor simulation code

--- 2020InfiniteRecharge/src/snobot_sim/java/com/girlsofsteel/simulator/BallSensorSimulator.java
+++ 2020InfiniteRecharge/src/snobot_sim/java/com/girlsofsteel/simulator/BallSensorSimulator.java
@@ -4,28 +4,37 @@ import com.snobot.simulator.SensorActuatorRegistry;
 import com.snobot.simulator.module_wrapper.interfaces.IDigitalIoWrapper;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import frc.robot.Constants;
 
 public class BallSensorSimulator {
 
 
     private final NetworkTable m_networkTable;
-//    private final IDigitalIoWrapper m_wrapper;
+    private final IDigitalIoWrapper m_handoffWrapper;
+    private final IDigitalIoWrapper m_secondaryWrapper;
+    private final IDigitalIoWrapper m_topWrapper; 
 
     public BallSensorSimulator()
     {
         m_networkTable = NetworkTableInstance.getDefault().getTable("BallSensorSim");
-        m_networkTable.getEntry("A").setBoolean(false);
-        m_networkTable.getEntry("B").setBoolean(false);
-        m_networkTable.getEntry("C").setBoolean(false);
+        m_networkTable.getEntry("Handoff").setBoolean(false);
+        m_networkTable.getEntry("Secondary").setBoolean(false);
+        m_networkTable.getEntry("Top").setBoolean(false);
 
-//        m_wrapper = SensorActuatorRegistry.get().getDigitalSources().get(0);
+        m_handoffWrapper = SensorActuatorRegistry.get().getDigitalSources().get(Constants.DIGITAL_INPUT_SENSOR_HANDOFF);
+        m_secondaryWrapper = SensorActuatorRegistry.get().getDigitalSources().get(Constants.DIGITAL_INPUT_SENSOR_SECONDARY); 
+        m_topWrapper = SensorActuatorRegistry.get().getDigitalSources().get(Constants.DIGITAL_INPUT_SENSOR_TOP); 
+
     }
 
-    public void update()
-    {
-        boolean buttonA = m_networkTable.getEntry("A").getBoolean(false);
-        boolean buttonB = m_networkTable.getEntry("B").getBoolean(false);
-        boolean buttonC = m_networkTable.getEntry("C").getBoolean(false);
-        System.out.println(buttonA + ", " + buttonB + ", " + buttonC);
+    public void update() {
+        boolean handOffSensor = m_networkTable.getEntry("Handoff").getBoolean(false);
+        boolean secondarySensor = m_networkTable.getEntry("Secondary").getBoolean(false);
+        boolean topSensor = m_networkTable.getEntry("Top").getBoolean(false);
+        System.out.println(handOffSensor + ", " + secondarySensor + ", " + topSensor);
+
+        m_handoffWrapper.set(handOffSensor);
+        m_secondaryWrapper.set(secondarySensor); 
+        m_topWrapper.set(topSensor); 
     }
 }
